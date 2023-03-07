dc9c5c4c9635b476643438a67a9337ac9b9f347d
srvr-au <60254275+srvr-au@users.noreply.github.com>
2023-03-07 17:36:22 +1100

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ mkdir /root/bums
 cd /root/bums
 
 echo 'Download and install srvr public key'
-if wget https://srvr-au.bitbucket.io/verifyscript.pubkey && gpg --import verifyscript.pubkey; then
+if wget https://srvr-au.bitbucket.io/verifyscript.pubkey &&
+gpg --import verifyscript.pubkey; then
   rm verifyscript.pubkey
 else
   echo 'Something went wrong. Exiting 1.'
@@ -13,7 +14,9 @@ else
 fi
 
 echo 'Download first install file and verify gpg signature'
-if wget https://cdn.jsdelivr.net/gh/srvr-au/bums/install1.sh && wget https://cdn.jsdelivr.net/gh/srvr-au/bums/gpgsigs/install1.sig && gpg --verify install1.sig install1.sh; then
+if wget https://raw.githubusercontent.com/srvr-au/bums/main/install1.sh && 
+wget https://raw.githubusercontent.com/srvr-au/bums/main/gpgsigs/install1.sig &&
+gpg --verify install1.sig install1.sh; then
   rm install1.sig
   chmod +x install1.sh
 else
--- install1.sh
+++ install1.sh
@@ -4,14 +4,14 @@ vtext='1.00'
 usetext='This script is requires a clean
   install of Ubuntu, at least version 22.04.
   This script sets up a simple basic server
-  with the option of a basic email capacity (s-nail),
+  with the option of a basic email capacity (msmtp-mta and mailx),
   You will need to run install2.sh if you want a fully
-  fledged MTA (Postfix). s-nail is best used in
+  fledged MTA (Postfix). msmtp is best used in
   scenarios that do not need to receive mail,
   just send occasional mail. Examples
   include DNS Server, Backup or other storage
   Server, SQL Server etc.
-  s-nail requires smtp settings such as an email
+  msmtp requires smtp settings such as an email
   username, password, hostname, and SSL port (587).'
 
 read -r -d '' htext <<-EOF
@@ -39,17 +39,14 @@ if [[ $( pwd ) != '/root/bums' ]]; then
   mkdir /root/bums
   cd /root/bums
 fi
-if [[ -f bashTK ]]; then
-  echo 'This script can only run once.'
-  exit
-fi
+[[ -f bashTK ]] && { echo 'This script can only run once.'; exit; }
 
 echo -e "Please wait while I grab a needed file...\n\n"
-if wget https://cdn.jsdelivr.net/gh/srvr-au/bashTK@main/bashTK &&
-  wget https://cdn.jsdelivr.net/gh/srvr-au/bashTK@main/bashTK.sig &&
+if wget https://raw.githubusercontent.com/srvr-au/bashTK/main/bashTK &&
+  wget https://raw.githubusercontent.com/srvr-au/bashTK/main/bashTK.sig &&
   gpg --verify bashTK.sig bashTK; then
   rm bashTK.sig
-  echo 'File downloaded and verified, press any key to clear and continue'
+  echo -e "\n\nFile downloaded and verified, press any key to clear and continue"
   read -n 1 -s
   clear
 else
@@ -60,14 +57,16 @@ fi
 source bashTK
 echo -e "${btkBlu}========================${btkRes}\n${btkBlu}Bash Ubuntu Management Scripts (BUMS)${btkRes}\n${btkBlu}========================${btkRes}\n"
 echo -e "${usetext}\n\n"
+BTKpause
 
 BTKheader 'Initial Server Check'
 thisOS=$( lsb_release -is )
 thisVer=$( lsb_release -rs )
 echo -e "This software is best run on a clean install of Ubuntu, version greater than 22.03"
-[[ $thisOS == 'Ubuntu' ]] && echo -e "${btkGre}Good, looks like we are running Ubuntu.${btkRes}" || BTKfatalError "The OS is not Ubuntu"
-[[ $(bc -l <<< "$thisVer > 22.03") -eq 1 ]] && echo -e "${btkGre}Good, looks like we are running the required version.${btkRes}" || BTKfatalError "The Version needs to be greater than 22.04"
-BTKcontinue
+[[ $thisOS == 'Ubuntu' ]] && BTKsuccess 'Good, looks like we are running Ubuntu.' || BTKfatalError "The OS is not Ubuntu"
+[[ $(bc -l <<< "$thisVer > 22.03") -eq 1 ]] && BTKsuccess 'Good, looks like we are running the required version.' || BTKfatalError "The Version needs to be greater than 22.04"
+BTKpause
+
 BTKinfo 'Time to update our repository information...'
 apt update
 BTKcmdCheck 'Update Repository'
@@ -129,22 +128,29 @@ echo 'Looks like it is time to install a few packages...'
 install=()
 BTKask 'Would you like to install sysstat (System Statistics)... ?'
 [[ ${btkYN} == 'y' ]] && install+=('sysstat') || echo 'No sysstat for you then...'
-BTKask 'Would you like to install s-nail (simple server email), rather than Postfix... ?'
-[[ ${btkYN} == 'y' ]] && install+=('s-nail') || echo 'You will need to run install2.sh to install Postfix...'
-echo -e "We will install the following software\n${install[@]}\n"
+BTKask 'Would you like to install msmtp-mta (simple server email), rather than Postfix... ?'
+if [[ ${btkYN} == 'y' ]]; then 
+  install+=('msmtp-mta bsd-mailx')
+  BTKask 'Would you like to install logwatch... ?'
+  [[ ${btkYN} == 'y' ]] && install+=('logwatch') || echo 'No Logwatch for you then...'
+else
+  echo 'You will need to run install2 after reboot in order to install Postfix. Every Server needs an MTA...'
+fi
+echo -e "We will try to install the following software\n${install[@]}\n"
 BTKpause
 BTKinstall ${install[@]}
 BTKpause
 
-if [[ "$installThese" == *"sysstat"* ]]; then
+if BTKisInstalled 'sysstat'; then
   echo 'Just enabling sysstat'
   BTKenable 'sysstat'
   BTKgetStatus 'sysstat'
   BTKpause
 fi
 
-if [[ "$installThese" == *"s-nail"* ]]; then
-  echo 'Mail on this server will be sent to an SMTP Server for delivery...'
+if BTKisInstalled 'msmtp-mta'; then
+  BTKheader 's-nail configuration'
+  BTKinfo "Mail on this server will be sent to an SMTP Server for delivery...${btkReturn}You will need hostname, username and password as well as port number (usually 587)."
   BTKaskConfirm 'SMTP Server Hostname'
   mtahost=$btkAnswerEng
   BTKaskConfirm 'SMTP Server Username'
@@ -159,55 +165,43 @@ if [[ "$installThese" == *"s-nail"* ]]; then
     mtatype='smtps'
     mtaport='465'
   fi
-  BTKaskConfirm 'All email is sent to root, root email address'
+  BTKinfo 'All email is sent to root, what email address will root send to?'
+  BTKaskConfirm 'Root email address'
   mtaroot=$btkAnswerEng
-  BTKaskConfirm 'From email address'
-  mtafrom=$btkAnswerEng
+  BTKaskConfirm 'From email address, y for root email or input another email address.'
+  [[ $btkAnswerEng == 'y' ]] && mtafrom=$mtaroot || mtafrom=$btkAnswerEng
+  
+  echo "
+defaults
+############
+tls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+syslog on
 
-  urlencodeduser=$( python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1]))" $mtauser )
-  urlencodedpass=$( python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1]))" $mtapass )
+account srvr
+###############
+host ${mtahost}
+port ${mtaport}
+from ${mtafrom}
+auth on
+user ${mtauser}
+password ${mtapass}
+
+account default : srvr
+
+aliases /etc/aliases
+" >> /root/.msmtprc
+  chmod 600 /root/.msmtprc
+  BTKcmdCheck 'chmod /root/.msmtprc 600.'
 
   echo "
-#--------------------------------------------#
-# Configure s-nail version v14.9.23            #
-#--------------------------------------------#
-
-# Testing syntax:
-# echo "Testing, Testing, Testing" | mail -s "My test..." recipEmailAddr
-# -v for verbose -vv for very verbose
-
-# Use v15.0 compatibility mode
-set v15-compat
-set mta-aliases=/etc/aliases
-
-# Essential setting: select allowed character sets
-set sendcharsets=utf-8,iso-8859-1
-# and reply in the same charset used by sender:
-set reply-in-same-charset
-
-# Default directory where we act in (relative to HOME)
-set folder=mail
-
-# Request strict TLS transport layer security checks
-set tls-verify=strict
-set tls-ca-file=/etc/ssl/certs/ca-certificates.crt
-set tls-ca-no-defaults
-set smtp-use-starttls
-set smtp-auth=login
-
-#set verbose
-set sendwait
-set from="${$mtafrom}"
-set mta=${mtatype}://${urlencodeduser}:${urlencodedpass}@${$mtahost}:${mtaport}
-
-#--------------------------------------------#
-" >> /root/.mailrc
+set sendmail="/usr/bin/msmtp"
+set mta="/usr/bin/msmtp"
+  " >> /root/.mailrc
   BTKcmdCheck 'configure /root/.mailrc'
 
   chmod 600 /root/.mailrc
   BTKcmdCheck 'chmod /root/.mailrc 600.'
-  ln -s /usr/bin/s-nail /usr/bin/mail
-  BTKcmdCheck 'link s-nail to mail command.'
 
   echo "
 root: $mtaroot
@@ -215,6 +209,23 @@ default: root
 " >> /etc/aliases
   BTKcmdCheck 'Write root email address into aliases file.'
   
+  BTKpause
+  
+  if BTKisInstalled 'logwatch'; then
+    BTKheader 'Configure Logwatch'
+    mkdir /var/cache/logwatch
+    echo 'Output = mail
+Format = text
+MailTo = root
+Range = yesterday
+Detail = low
+Service = All
+' > /etc/logwatch/conf/logwatch.conf
+  fi
+  BTKcmdCheck 'logwatch configured...'
+  
+  BTKpause
+  BTKheader 'Configure Unattended Upgrades.'
   echo '
 Unattended-Upgrade::Mail "root";
 Unattended-Upgrade::MailReport "always";
@@ -222,13 +233,19 @@ Unattended-Upgrade::Remove-Unused-Dependencies "true";
 Unattended-Upgrade::Automatic-Reboot "false";
 ' >> /etc/apt/apt.conf.d/50unattended-upgrades
 
-  BTKcmdCheck 'Enable unattended upgrades to send mail and reboot'
+  BTKcmdCheck 'Enable unattended upgrades to send mail and not reboot'
   
+  BTKpause
+  BTKheader 'Install cron jobs...'
   mkdir -p /root/bums/cron/graphs
   BTKcmdCheck 'Make cron/graphs Directory.'
 
-  if wget https://cdn.jsdelivr.net/gh/srvr-au/bums/cron/sysstatReport.sh; then
-    BTKsuccess 'sysstatReport.sh downloaded...'
+  if BTKisInstalled 'sysstat'; then
+  if wget https://raw.githubusercontent.com/srvr-au/bums/main/cron/sysstatReport.sh &&
+  wget https://raw.githubusercontent.com/srvr-au/bums/main/gpgsigs/sysstatReport.sig &&
+  gpg --verify sysstatReport.sig sysstatReport.sh; then
+    rm sysstatReport.sig
+    BTKsuccess 'sysstatReport.sh downloaded and verified...'
     mv sysstatReport.sh cron/sysstatReport.sh
     BTKcmdCheck 'Move sysstatReport.sh to cron directory.'
     chmod +x cron/sysstatReport.sh
@@ -240,14 +257,18 @@ Unattended-Upgrade::Automatic-Reboot "false";
   else
     BTKerror 'sysstatReport.sh failed to download.'
   fi
+  fi
 
-  if wget https://cdn.jsdelivr.net/gh/srvr-au/bums/cron/rebootCheck.sh; then
-    BTKsuccess 'rebootCheck.sh downloaded...'
+  if wget https://raw.githubusercontent.com/srvr-au/bums/main/cron/rebootCheck.sh &&
+  wget https://raw.githubusercontent.com/srvr-au/bums/main/gpgsigs/rebootCheck.sig &&
+  gpg --verify rebootCheck.sig rebootCheck.sh; then
+    rm rebootCheck.sig
+    BTKsuccess 'rebootCheck.sh downloaded and verified...'
     mv rebootCheck.sh cron/rebootCheck.sh
     BTKcmdCheck 'Move rebootCheck.sh to cron directory.'
     chmod +x cron/rebootCheck.sh
     BTKcmdCheck 'chmod rebootCheck.sh executable.'
-    command="/root/bums/cron/rebootcheck.sh > /dev/null 2>&1"
+    command="/root/bums/cron/rebootCheck.sh > /dev/null 2>&1"
     job="@reboot $command"
     BTKmakeCron "$command" "$job"
     BTKcmdCheck 'rebootCheck.sh cron installation'
@@ -255,8 +276,11 @@ Unattended-Upgrade::Automatic-Reboot "false";
     BTKerror 'rebootCheck.sh failed to download...'
   fi
 
-  if wget https://cdn.jsdelivr.net/gh/srvr-au/bums/cron/rblCheck.sh; then
-    BTKsuccess 'rblCheck.sh downloaded...'
+  if wget https://raw.githubusercontent.com/srvr-au/bums/main/cron/rblCheck.sh &&
+  wget https://raw.githubusercontent.com/srvr-au/bums/main/gpgsigs/rblCheck.sig &&
+  gpg --verify rblCheck.sig rblCheck.sh; then
+    rm rblCheck.sig
+    BTKsuccess 'rblCheck.sh downloaded and verified...'
     mv rblCheck.sh cron/rblCheck.sh
     BTKcmdCheck 'Move rblCheck.sh to cron directory.'
     chmod +x cron/rblCheck.sh
@@ -272,34 +296,42 @@ Unattended-Upgrade::Automatic-Reboot "false";
   else
     echo 'Every Server needs some way to send mail - so you will need to install Postfix.'
     echo 'Downloading install2.sh, run it after reboot...'
-    if wget https://cdn.jsdelivr.net/gh/srvr-au/bums/install2.sh; then
+    if wget https://raw.githubusercontent.com/srvr-au/bums/main/install2.sh; then
       BTKsuccess 'install2.sh download successful.'
       chmod +x install2.sh
       BTKcmdCheck 'chmod install2.sh executable'
     else
       echo 'install2.sh download failed.'
     fi
-  BTKpause
   fi
 BTKpause
-fi
 
 BTKheader 'Uncomplicated Firewall open ssh and enable.'
-if ufw allow OpenSSH; then
-  BTKsuccess 'Firewall allow OpenSSH.'
-  if ufw enable; then
-    BTKsuccess 'Firewall enabled.'
-    ufw status
+if BTKisInstalled 'ufw'; then
+  BTKask 'You have Uncomplicated Firewall (UFW) installed, do you want to allow OpenSSH and Enable.'
+  if [[ $btkYN == 'y' ]]; then
+    if ufw allow OpenSSH; then
+      BTKsuccess 'Firewall allow OpenSSH.'
+      # echo "y" | ufw enable
+      if ufw --force enable; then
+        BTKsuccess 'Firewall enabled.'
+        ufw status
+      else
+        BTKerror 'Firewall failed to enable.'
+      fi
+    else
+      BTKerror 'Firewall failed to allow OpenSSH and NOT enabled.'
+    fi
   else
-    BTKerror 'Firewall failed to enable.'
+    echo 'OK, no Uncomplicated Firewall for you then...'
   fi
 else
-  BTKerror 'Firewall failed to allow OpenSSH and NOT enabled.'
+  BTKwarn 'Uncomplicated firewall not installed.'
 fi
 BTKpause
 
 BTKheader 'Finish: Upgrade and Reboot'
 echo 'Now we will upgrade all Server Software... then reboot'
 BTKpause
-apt full-upgrade -y
+DEBIAN_FRONTEND=noninteractive apt full-upgrade -y
 systemctl reboot
